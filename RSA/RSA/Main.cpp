71b60053d6d4d07014d8bbd35d6bd994de6fca4e
Mike <38372845+mbernier1@users.noreply.github.com>
2021-02-16 18:22:25 -0800

    Added cout statements for keys

---
+++ RSA/RSA/Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+
+
+
+int main()
+{
+	int keyE, keyN, keyD = 0;
+	
+	cout << "Input for Key E: " << endl;
+	cin >> keyE;
+
+	cout << "Input for Key N: " << endl;
+	cin >> keyN;
+
+	cout << "Input for Key D: " << endl;
+	cin >> keyD;
+	
+}
