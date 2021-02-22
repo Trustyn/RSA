34bc081247b5f08d5ee0fa18db63b89e99dc032e
Mike <38372845+mbernier1@users.noreply.github.com>
2021-02-21 20:25:49 -0800

    still trying

--- RSA/RSA/Main.cpp
+++ RSA/RSA/Main.cpp
@@ -4,8 +4,8 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int gcd(int a, int b) {
-	int t;
+unsigned long gcd(unsigned long a, unsigned long b) {
+	unsigned long t;
 	while (1) {
 		t = a % b;
 		if (t == 0)
@@ -15,57 +15,62 @@ int gcd(int a, int b) {
 	}
 }
 
-int main()
+unsigned long main()
 {	//KEEP PRIVATE
 	//Select 2 large primes 
-	int largePrime_P = 10007;
-	int largePrime_Q = 29881;
+	unsigned long largePrime_P = 10007;
+	//unsigned long largePrime_P = 13;
+	unsigned long largePrime_Q = 29881;
+	//unsigned long largePrime_Q = 11;
 
 	//PUBLIC KEY N
 	//Compute N
-	int publicKey_N = largePrime_P * largePrime_Q;
+	unsigned long publicKey_N = largePrime_P * largePrime_Q;
 	//10007 * 29881 = 299,019,167
 
 	//Compute Totient Phi(N) = (p-1)*(q-1)
-	long int phi = (largePrime_P - 1) * (largePrime_Q - 1);
+	unsigned int phi = (largePrime_P - 1) * (largePrime_Q - 1);
 	//(10007 - 1) = 10006 || (29881 - 1) = 29880 || 10006 * 29880 = 298,979,280
 
 	//PUBLIC KEY E
 	//e such that (1 < e < phi), gcd(e,phi(N)) = 1
-	int publicKey_E = 7;
-	int track;
-
-	while (publicKey_E < phi) {
-		track = gcd(publicKey_E, phi);
-		if (track == 1)
-			break;
-		else
-			publicKey_E++;
-	}
-
+	unsigned long publicKey_E = 7;
+	
 	//KEEP PRIVATE
 	//Decryption Key (private key) using inverse GCD
 	//(e * d) mod phi(N) = 1 and (0 <= d <= N)
-	int privateKey_D;
-	int arbitraryValue = 2;
+	double privateKey_D;
+	unsigned long track;
 
-	privateKey_D = (1 + (arbitraryValue * phi)) / publicKey_E;
-	// (1 + (2 * 298,979,280)) / 7 = 85,422,651
+	privateKey_D = (1 + (2 * phi)) / publicKey_E;
+	/*for (unsigned long i = 1; i < publicKey_N; i++)
+	{
+		track = fmod((publicKey_E * i), phi);
+		if (track == 1)
+		{
+			cout << i << endl;
+			privateKey_D = i;
+			break;
+		}
+	}*/
+	
+	//privateKey_D = fmod((publicKey_E * privateKey_D), phi);
+	// (7 * 170845303) % 298,979,280
 
 
 	//Publish public keys
 	cout << "Key E is: " << publicKey_E << endl;
 	cout << "Key N is: " << publicKey_N << endl;
 
-	int message_P = 0;
+	unsigned long message_P = 0;
 
 	//LOOP TO CHECK Message_P is smaller than mod publicKey_N
 	//Ask user for message to encrypt such that M(message) is smaller than modulus N
 	cout << "Please enter your message: ";
 	cin >> message_P;
 
-	int userPublicKey_E = 0;
-	int userPublicKey_N = 0;
+	unsigned long userPublicKey_E = 0;
+	unsigned long userPublicKey_N = 0;
 	
 	//Collect public keys from user
 	cout << "Please enter key E: ";
@@ -77,11 +82,11 @@ int main()
 	cout << endl;
 
 	//Compute to encrypt user plaintext.
-	long int ciphertext = 1;
-
-	//ciphertext = pow(message_P, userPublicKey_E);
+	unsigned long ciphertext = 1;// pow(message_P, userPublicKey_E);
+	//23 * 7
 	//ciphertext = fmod(ciphertext, userPublicKey_N);
-	for (int i = 0; i < userPublicKey_E; ++i) {
+	//161 % 299,019,167
+	for (unsigned long i = 0; i < userPublicKey_E; ++i) {
 		ciphertext = fmod((ciphertext * message_P), publicKey_N);
 	}
 
@@ -89,7 +94,7 @@ int main()
 	cout << "Your ciphertext is: " << ciphertext << endl;
 	cout << endl;
 
-	int message_C = 0;
+	unsigned long message_C = 0;
 
 	//Collect ciphertext
 	cout << "Please enter your ciphertext message: ";
@@ -97,11 +102,12 @@ int main()
 	cout << endl;
 
 	//Compute to decrypt user ciphertext
-	long int plaintext = 1;
+	unsigned long plaintext = 1;// pow(message_C, privateKey_D);
 
 	//plaintext = pow(message_C, privateKey_D);
+	// 115614610 ^ 170845303
 	//plaintext = fmod(plaintext, publicKey_N);
-	for (int i = 0; i < privateKey_D; ++i) {
+	for (unsigned long i = 0; i < privateKey_D; ++i) {
 		plaintext = fmod((plaintext * message_C), publicKey_N);
 	}
 
