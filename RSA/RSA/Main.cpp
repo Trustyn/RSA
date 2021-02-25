e0cc6b7c703d41b3910c92d24bc0cb54cae224aa
Mike <38372845+mbernier1@users.noreply.github.com>
2021-02-24 18:04:32 -0800

    Finalized Project
    Took out non essential comments and cleaned up white space

--- RSA/RSA/Main.cpp
+++ RSA/RSA/Main.cpp
@@ -16,47 +16,35 @@ unsigned long gcd(unsigned long a, unsigned long b) {
 }
 
 unsigned long main()
-{	//KEEP PRIVATE
-	//Select 2 large primes 
+{	
 	unsigned long largePrime_P = 10007;
-	//unsigned long largePrime_P = 13;
-	unsigned long largePrime_Q = 29881;
-	//unsigned long largePrime_Q = 11;
+	unsigned long largePrime_Q = 10009;
 
 	//PUBLIC KEY N
-	//Compute N
 	unsigned long publicKey_N = largePrime_P * largePrime_Q;
-	//10007 * 29881 = 299,019,167
 
 	//Compute Totient Phi(N) = (p-1)*(q-1)
 	unsigned int phi = (largePrime_P - 1) * (largePrime_Q - 1);
-	//(10007 - 1) = 10006 || (29881 - 1) = 29880 || 10006 * 29880 = 298,979,280
 
 	//PUBLIC KEY E
-	//e such that (1 < e < phi), gcd(e,phi(N)) = 1
 	unsigned long publicKey_E = 7;
 	
 	//KEEP PRIVATE
 	//Decryption Key (private key) using inverse GCD
-	//(e * d) mod phi(N) = 1 and (0 <= d <= N)
-	double privateKey_D;
+	unsigned long privateKey_D = 1;
 	unsigned long track;
 
-	privateKey_D = (1 + (2 * phi)) / publicKey_E;
-	/*for (unsigned long i = 1; i < publicKey_N; i++)
+	for (unsigned long i = 1; i < publicKey_N; i++)
 	{
 		track = fmod((publicKey_E * i), phi);
 		if (track == 1)
 		{
 			cout << i << endl;
 			privateKey_D = i;
+			cout << privateKey_D << endl;
 			break;
 		}
-	}*/
-	
-	//privateKey_D = fmod((publicKey_E * privateKey_D), phi);
-	// (7 * 170845303) % 298,979,280
-
+	}
 
 	//Publish public keys
 	cout << "Key E is: " << publicKey_E << endl;
@@ -64,7 +52,6 @@ unsigned long main()
 
 	unsigned long message_P = 0;
 
-	//LOOP TO CHECK Message_P is smaller than mod publicKey_N
 	//Ask user for message to encrypt such that M(message) is smaller than modulus N
 	cout << "Please enter your message: ";
 	cin >> message_P;
@@ -72,22 +59,12 @@ unsigned long main()
 	unsigned long userPublicKey_E = 0;
 	unsigned long userPublicKey_N = 0;
 	
-	//Collect public keys from user
-	cout << "Please enter key E: ";
-	cin >> userPublicKey_E;
-	cout << endl;
-
-	cout << "Please enter key N: ";
-	cin >> userPublicKey_N;
-	cout << endl;
 
 	//Compute to encrypt user plaintext.
-	unsigned long ciphertext = 1;// pow(message_P, userPublicKey_E);
-	//23 * 7
-	//ciphertext = fmod(ciphertext, userPublicKey_N);
-	//161 % 299,019,167
-	for (unsigned long i = 0; i < userPublicKey_E; ++i) {
-		ciphertext = fmod((ciphertext * message_P), publicKey_N);
+	unsigned long ciphertext = 1;
+	
+	for (unsigned long i = 0; i < publicKey_E; ++i) {
+		ciphertext = (ciphertext * message_P) % publicKey_N;
 	}
 
 	//Output Ciphertext
@@ -102,13 +79,11 @@ unsigned long main()
 	cout << endl;
 
 	//Compute to decrypt user ciphertext
-	unsigned long plaintext = 1;// pow(message_C, privateKey_D);
+	unsigned long plaintext = 1;
 
-	//plaintext = pow(message_C, privateKey_D);
-	// 115614610 ^ 170845303
-	//plaintext = fmod(plaintext, publicKey_N);
+	
 	for (unsigned long i = 0; i < privateKey_D; ++i) {
-		plaintext = fmod((plaintext * message_C), publicKey_N);
+		plaintext = (plaintext * message_C) % publicKey_N;
 	}
 
 	//Output Plaintext
